270008614f5e2d5afad55e17e1ca942177efc718
Mark DePristo <mdepristo@google.com>
2015-07-22 14:05:17 -0700

    Revert changes after merge

--- pygg/__init__.py
+++ pygg/__init__.py
@@ -1,5 +1,1 @@
-# This isn't safe at all.  __init__.py is loaded when
-# you do an import pygg.  Are you trying to automatically
-# bring in all of the individual symbols?  If so, that's not a good
-# idea in general.  Let's discuss but disabling now
-# from pygg import *
+from pygg import *
