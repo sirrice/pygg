707f340d09d1c22579ba8a86d16397428d6b7cfa
Ivan Kryukov <ivan.o.kryukov@gmail.com>
2017-01-16 15:52:31 -0700

    lifted to ggplot 2.2.1 and changed module import mechanism

--- pygg/__init__.py
+++ pygg/__init__.py
@@ -1,1 +1,9 @@
+from ggplot2_functions import ggplot2_functions
+from pygg import make_ggplot2_binding
+
+for f in ggplot2_functions:
+    exec('%s = make_ggplot2_binding("%s")' % (f, f))
+
 from pygg import *
+
+__all__ = ['esc', 'is_escaped', 'ggsave', 'facet_wrap', 'facet_grid'] + ggplot2_functions
