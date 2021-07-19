f7c8b5292e29eb33c73e1a155baf0ef81c096e25
Eugene Wu <eugenewu@dyn-160-39-244-233.dyn.columbia.edu>
2021-07-19 10:53:31 -0400

    change iris dataset to unicode in tests

--- tests/pygg_tests.py
+++ tests/pygg_tests.py
@@ -244,7 +244,7 @@ class TestIntegration(unittest.TestCase):
             pygg.ggsave(tmpfile, p, data=None, quiet=True)
 
 
-IRIS_DATA_CSV = """SepalLength,SepalWidth,PetalLength,PetalWidth,Name
+IRIS_DATA_CSV = u"""SepalLength,SepalWidth,PetalLength,PetalWidth,Name
 5.1,3.5,1.4,0.2,Iris-setosa
 4.9,3.0,1.4,0.2,Iris-setosa
 4.7,3.2,1.3,0.2,Iris-setosa
